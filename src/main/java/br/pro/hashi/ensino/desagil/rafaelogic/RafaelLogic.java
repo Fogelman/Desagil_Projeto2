a91f496e595967ea08404cd52681025f781a60d3
Bruno Costa <brunex887@gmail.com>
2018-09-24 18:19:55 -0300

    Imagens e circulo c com feitos

--- src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/RafaelLogic.java
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/RafaelLogic.java
@@ -12,10 +12,8 @@ import br.pro.hashi.ensino.desagil.rafaelogic.model.OrGate;
 import br.pro.hashi.ensino.desagil.rafaelogic.model.XorGate;
 import br.pro.hashi.ensino.desagil.rafaelogic.view.View;
 
-
-
 public class RafaelLogic {
-	
+
 	public static void main(String[] args) {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			@Override
@@ -26,22 +24,17 @@ public class RafaelLogic {
 				model.add(new NotGate());
 				model.add(new OrGate());
 				model.add(new XorGate());
-				model.add(new NandGate());
-				
-				
 
 				View view = new View(model);
 
 				JFrame frame = new JFrame();
-            	frame.setContentPane(view);
-            	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            	frame.setResizable(false);
-            	frame.pack();
-            	frame.setVisible(true);
+				frame.setContentPane(view);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setResizable(false);
+				frame.pack();
+				frame.setVisible(true);
 			}
 		});
 	}
 
 }
-
-
--- src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/view/BooleanView.java
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/view/BooleanView.java
@@ -1,65 +1,58 @@
 package br.pro.hashi.ensino.desagil.rafaelogic.view;
 
 import java.awt.Color;
-
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.net.URL;
 
 import javax.swing.JColorChooser;
 import javax.swing.JLabel;
+import javax.swing.ImageIcon;
 import javax.swing.JCheckBox;
 
 import br.pro.hashi.ensino.desagil.rafaelogic.model.Gate;
 import br.pro.hashi.ensino.desagil.rafaelogic.model.Source;
 
-// Duas modificações em relação à versão da entrega anterior:
-// (a) esta classe agora é subclasse de SimplePanel em vez
-// de JPanel; e (b) esta classe agora implementa MouseListener,
-// indicando que ela reage a eventos de interação com o mouse.
 public class BooleanView extends SimplePanel implements ActionListener, MouseListener {
 	private static final long serialVersionUID = 1L;
 
 	private Gate gate;
 	private JCheckBox aField;
 	private JCheckBox bField;
-	private JCheckBox resultField;
+	//private JCheckBox resultField;
 
-	// Estes nomes aqui são auto-explicativos, não?
 	private Color color;
+	private Image image;
+	private boolean result;
 
 	public BooleanView(Gate gate) {
 
-		// Como subclasse de SimplePanel, esta classe agora
-		// exige que uma largura e uma altura sejam fixadas.
-		super(150, 150);
+		super(300, 300);
 
 		this.gate = gate;
 
 		aField = new JCheckBox();
 		bField = new JCheckBox();
-		resultField = new JCheckBox();
+		//resultField = new JCheckBox();
 
 		JLabel entradaLabel = new JLabel("Entrada:");
 
-		JLabel resultLabel = new JLabel("Saida:");
+		//JLabel resultLabel = new JLabel("Saida:");
 
-		// Tiramos a chamada de setLayout, ou todo o trabalho feito
-		// pela superclasse SimplePanel teria sido jogado no lixo!
-
-		// Como subclasse de SimplePanel, agora podemos definir a
-		// posição e o tamanho de cada componente ao adicioná-la.
 		add(entradaLabel, 10, 10, 75, 25);
 		add(aField, 10, 30, 150, 25);
-
-		add(resultLabel, 10, 80, 75, 25);
-		add(resultField, 10, 100, 150, 25);
+		
+		//add(resultLabel, 10, 80, 75, 25);
+		//add(resultField, 10, 100, 20, 20);
 
 		aField.addActionListener(this);
 		bField.addActionListener(this);
 
-		resultField.setEnabled(false);
+		//resultField.setEnabled(false);
 
 		if (isNotGate()) {
 			add(bField, 10, 50, 75, 25);
@@ -68,16 +61,11 @@ public class BooleanView extends SimplePanel implements ActionListener, MouseLis
 
 		update();
 
-		// Usamos isso no Projeto 1, vocês lembram?
-//		String path = "/" + calculator.toString() + ".png";
-//		URL url = getClass().getResource(path);
-//		image = new ImageIcon(url).getImage();
+		String path = "/" + gate.toString() + ".png";
+		System.out.println(path);
+		URL url = getClass().getResource(path);
+		image = new ImageIcon(url).getImage();
 
-		// Toda componente Swing possui este método, que
-		// permite adicionar objetos que reagem a eventos
-		// de mouse que acontecem nela. Ou seja, ao passar
-		// this como parâmetro, estamos pedindo para a
-		// componente reagir aos próprios eventos de mouse.
 		addMouseListener(this);
 	}
 
@@ -100,8 +88,8 @@ public class BooleanView extends SimplePanel implements ActionListener, MouseLis
 			gate.connect(0, pinA);
 		}
 
-		boolean result = gate.read();
-		resultField.setSelected(result);
+		result = gate.read();
+		//resultField.setSelected(result);
 	}
 
 	private boolean isNotGate() {
@@ -115,50 +103,60 @@ public class BooleanView extends SimplePanel implements ActionListener, MouseLis
 		update();
 	}
 
-	// Conjunto de métodos que devem ser implementados por todo
-	// objeto do tipo MouseListener. Estamos interessados em
-	// reagir a um clique, ou seja, o ato de pressionar e soltar
-	// um botão do mouse, sem sair do lugar entre os dois momentos.
 	@Override
 	public void mouseClicked(MouseEvent event) {
 
-		// Descobre em qual posição o clique ocorreu.
 		int x = event.getX();
 		int y = event.getY();
+		System.out.println(x);
+		System.out.println(y);
+		double distancia = Math.pow((x-220), 2) + Math.pow((y-170), 2);
+		System.out.println(distancia);
+		if ( distancia <= 400) {
 
-		// Se o clique foi dentro do retângulo colorido...
-		if (x >= 195 && x < 235 && y >= 80 && y < 255) {
-
-			// ...então abrimos o seletor de cor...
 			color = JColorChooser.showDialog(this, null, color);
-
-			// ...e atualizamos a tela.
 			repaint();
 		}
 	}
 
 	@Override
 	public void mousePressed(MouseEvent event) {
-		// Não estamos interessados em reagir ao ato
-		// de apenas pressionar. Deixamos este vazio.
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent event) {
-		// Não estamos interessados em reagir ao ato
-		// de apenas soltar. Deixamos este vazio.
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent event) {
-		// Não estamos interessados em reagir ao ato
-		// do cursor entrar. Deixamos este vazio.
 	}
 
 	@Override
 	public void mouseExited(MouseEvent event) {
-		// Não estamos interessados em reagir ao ato
-		// do cursor sair. Deixamos este vazio.
 	}
 
+	@Override
+	public void paintComponent(Graphics g) {
+
+		super.paintComponent(g);
+
+		g.drawImage(image, 10, 80, 175, 175, null);
+
+		g.setColor(color);
+		
+		if (result) {
+			g.fillOval(200, 150, 40, 40);
+			repaint();
+
+		}
+		
+		else {
+			g.drawOval(200, 150, 40, 40);
+			repaint();
+		}
+
+
+		getToolkit().sync();
+	}
 }
